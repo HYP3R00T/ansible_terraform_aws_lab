18d866fd67235ae7f29fc921503d080a40679719
hyperoot <rajesh@hyperoot.dev>
2025-11-18 17:47:19 +0000

    Add egress rule to security group for allowing all outbound traffic

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -25,6 +25,14 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "ansible-lab-sg"
   }
